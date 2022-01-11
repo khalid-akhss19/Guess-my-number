49fe2668fe85841d3c86ed46628fe275f4a65978
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2022-01-11 12:57:08 +0500

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Guess My Number Project
+
+Simple JS DOM project from **Jonas Udemy Course**  
---
+++ script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+//commented orignal code
+
+/*
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+  document.querySelector('.message'). textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number');
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // when guess is wrong
+  } else if(guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage((guess > secretNumber) ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// reset button
+const againBTN = document.querySelector('.again');
+
+//reset event handler
+againBTN.addEventListener('click', () => {
+  // reset score 
+  score = 20;
+  // reset secret number
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  //reset message,score and number to initial values
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  //reset css values
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+})  
+
+*/
+
+/*
+document.querySelector('.message').textContent = ' correct woow';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = ' 10';
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = ' 13'; */
+
+// aaaEvent Listner
+// document.querySelector('.check').addEventListener('click' function () {
+//   console,log(document.querySelector('.guess').value);
+// });
+
+// aaaEvent Listner
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+
+//   //document.querySelector('.message').textContent = 'good attempt !';
+// });
